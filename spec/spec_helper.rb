e641f13fff47d7966a9df0756404567fb218f704
Markus Schirp <mbj@schirp-dso.com>
2014-04-14 11:47:54 +0000

    Run all tests on each mutation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,9 +5,37 @@ if ENV['TRAVIS']
   require 'coveralls'
   Coveralls.wear!
 else
-  require 'simplecov'
-  SimpleCov.start
+# require 'simplecov'
+# SimpleCov.start
 end
 
 require 'naught'
 Dir[File.join(GEM_ROOT, 'spec', 'support', '**/*.rb')].each { |f| require f }
+
+require 'mutant'
+require 'mutant-rspec'
+
+# Monkeypatch to mutant all specs per mutation.
+#
+# TODO: Use master once it supports configurable implicit coverage.
+#
+# Morpher predicates are needed to finally make this configurable in mutant.
+#
+module Mutant
+
+  module Rspec
+    class Killer
+
+      # Return all example groups
+      #
+      # @return [Enumerable<RSpec::Example>]
+      #
+      # @api private
+      #
+      def example_groups
+        strategy.example_groups
+      end
+
+    end # Rspec
+  end # Killer
+end # Mutant
